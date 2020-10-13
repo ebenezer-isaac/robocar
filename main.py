afd943748ff0562d02ff9d97bb4ec4575e94e1df
Ebenezer Isaac <mail@ebenezer-isaac.com>
2020-10-14 05:24:30 +0530

    pwm first try

--- main.py
+++ main.py
@@ -5,23 +5,43 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(8, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(10, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(16, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+wheel1_pwm = GPIO.PWM(8,1000)
+wheel2_pwm = GPIO.PWM(10,1000)
+wheel3_pwm = GPIO.PWM(12,1000)
+wheel4_pwm = GPIO.PWM(16,1000)
+wheel1_pwm.start(100)
+wheel2_pwm.start(100)
+wheel3_pwm.start(100)
+wheel3_pwm.start(100)
+
 
 pygame.init()
 print("initialized")
 screen = pygame.display.set_mode((200,200))
 running = True
 direction = 0
+speed = 1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_PLUS]:
+    	if speed <5:
+    		speed = speed + 1
+    if keys[pygame.K_MINUS]:
+    	if speed >1:
+    		speed = speed - 1
     if keys[pygame.K_w]:
     	direction = 0
+    	wheel1.ChangeDutyCycle(duty)
+    	wheel2.ChangeDutyCycle(100-(speed*20))
+    	wheel3.ChangeDutyCycle(duty)
+    	wheel4.ChangeDutyCycle(100-(speed*20))
         GPIO.output(8, GPIO.HIGH)
         GPIO.output(10, GPIO.LOW)
         GPIO.output(12, GPIO.HIGH)
