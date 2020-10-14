8a4178153de0b53e716c9205317b50115065920e
Ebenezer Isaac <mail@ebenezer-isaac.com>
2020-10-14 16:13:47 +0530

    diagonal and pwm

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ wheel1_pwm = GPIO.PWM(8,1000)
 wheel2_pwm = GPIO.PWM(10,1000)
 wheel3_pwm = GPIO.PWM(12,1000)
 wheel4_pwm = GPIO.PWM(16,1000)
-wheel1_pwm.start(100)
-wheel2_pwm.start(100)
-wheel3_pwm.start(100)
-wheel3_pwm.start(100)
+wheel1_pwm.start(0)
+wheel2_pwm.start(0)
+wheel3_pwm.start(0)
+wheel4_pwm.start(0)
 
 
 pygame.init()
@@ -33,55 +33,77 @@ while running:
     if keys[pygame.K_PLUS]:
     	if speed <5:
     		speed = speed + 1
+        print(speed)
     if keys[pygame.K_MINUS]:
     	if speed >1:
     		speed = speed - 1
-    if keys[pygame.K_w]:
+        print(speed)
+    if keys[pygame.K_w] and keys[pygame.K_a]:
+    	direction = 0
+    	wheel1_pwm.ChangeDutyCycle(50)
+    	wheel2_pwm.ChangeDutyCycle(0)
+    	wheel3_pwm.ChangeDutyCycle(100)
+    	wheel4_pwm.ChangeDutyCycle(0)
+    elif keys[pygame.K_w] and keys[pygame.K_d]:
     	direction = 0
     	wheel1_pwm.ChangeDutyCycle(100)
-    	wheel2_pwm.ChangeDutyCycle(100-(speed*20))
+    	wheel2_pwm.ChangeDutyCycle(0)
     	wheel3_pwm.ChangeDutyCycle(100)
-    	wheel4_pwm.ChangeDutyCycle(100-(speed*20))
-#        GPIO.output(8, GPIO.HIGH)
-#        GPIO.output(10, GPIO.LOW)
-#        GPIO.output(12, GPIO.HIGH)
-#        GPIO.output(16, GPIO.LOW)
+    	wheel4_pwm.ChangeDutyCycle(0)
+    elif keys[pygame.K_s] and keys[pygame.K_a]:
+    	direction = 0
+    	wheel1_pwm.ChangeDutyCycle(0)
+    	wheel2_pwm.ChangeDutyCycle(50)
+    	wheel3_pwm.ChangeDutyCycle(0)
+    	wheel4_pwm.ChangeDutyCycle(100)
+    elif keys[pygame.K_s] and keys[pygame.K_d]:
+    	direction = 0
+    	wheel1_pwm.ChangeDutyCycle(0)
+    	wheel2_pwm.ChangeDutyCycle(100)
+    	wheel3_pwm.ChangeDutyCycle(0)
+    	wheel4_pwm.ChangeDutyCycle(50)
+    elif keys[pygame.K_w]:
+    	direction = 0
+    	wheel1_pwm.ChangeDutyCycle(100)
+    	wheel2_pwm.ChangeDutyCycle(0)
+    	wheel3_pwm.ChangeDutyCycle(100)
+    	wheel4_pwm.ChangeDutyCycle(0)
     elif keys[pygame.K_s]:
     	direction = 1
-        GPIO.output(8, GPIO.LOW)
-        GPIO.output(10, GPIO.HIGH)
-        GPIO.output(12, GPIO.LOW)
-        GPIO.output(16, GPIO.HIGH)
+    	wheel1_pwm.ChangeDutyCycle(0)
+    	wheel2_pwm.ChangeDutyCycle(100)
+    	wheel3_pwm.ChangeDutyCycle(0)
+    	wheel4_pwm.ChangeDutyCycle(100)
     elif keys[pygame.K_a]:
     	if direction==0:
-            GPIO.output(8, GPIO.LOW)
-            GPIO.output(10, GPIO.LOW)
-            GPIO.output(12, GPIO.HIGH)
-            GPIO.output(16, GPIO.LOW)
+            wheel1_pwm.ChangeDutyCycle(0)
+            wheel2_pwm.ChangeDutyCycle(0)
+            wheel3_pwm.ChangeDutyCycle(100)
+            wheel4_pwm.ChangeDutyCycle(0)
         elif direction==1:
-            GPIO.output(8, GPIO.LOW)
-            GPIO.output(10, GPIO.LOW)
-            GPIO.output(12, GPIO.LOW)
-            GPIO.output(16, GPIO.HIGH)
+            wheel1_pwm.ChangeDutyCycle(0)
+            wheel2_pwm.ChangeDutyCycle(0)
+            wheel3_pwm.ChangeDutyCycle(0)
+            wheel4_pwm.ChangeDutyCycle(100)
     elif keys[pygame.K_d]:
     	if direction==0:
-            GPIO.output(8, GPIO.HIGH)
-            GPIO.output(10, GPIO.LOW)
-            GPIO.output(12, GPIO.LOW)
-            GPIO.output(16, GPIO.LOW)
+            wheel1_pwm.ChangeDutyCycle(100)
+            wheel2_pwm.ChangeDutyCycle(0)
+            wheel3_pwm.ChangeDutyCycle(0)
+            wheel4_pwm.ChangeDutyCycle(0)
         elif direction==1:
-            GPIO.output(8, GPIO.LOW)
-            GPIO.output(10, GPIO.HIGH)
-            GPIO.output(12, GPIO.LOW)
-            GPIO.output(16, GPIO.LOW)
+            wheel1_pwm.ChangeDutyCycle(0)
+            wheel2_pwm.ChangeDutyCycle(100)
+            wheel3_pwm.ChangeDutyCycle(0)
+            wheel4_pwm.ChangeDutyCycle(0)
     elif keys[pygame.K_SPACE]:
-        GPIO.output(8, GPIO.HIGH)
-        GPIO.output(10, GPIO.HIGH)
-        GPIO.output(12, GPIO.HIGH)
-        GPIO.output(16, GPIO.HIGH)
+    	wheel1_pwm.ChangeDutyCycle(0)
+    	wheel2_pwm.ChangeDutyCycle(0)
+    	wheel3_pwm.ChangeDutyCycle(0)
+    	wheel4_pwm.ChangeDutyCycle(0)
     else:
-        GPIO.output(8, GPIO.HIGH)
-        GPIO.output(10, GPIO.HIGH)
-        GPIO.output(12, GPIO.HIGH)
-        GPIO.output(16, GPIO.HIGH)
+    	wheel1_pwm.ChangeDutyCycle(0)
+    	wheel2_pwm.ChangeDutyCycle(0)
+    	wheel3_pwm.ChangeDutyCycle(0)
+    	wheel4_pwm.ChangeDutyCycle(0)
 
