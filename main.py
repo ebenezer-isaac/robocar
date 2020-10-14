c111d262a80a30cecf8562b56e0cf059b821e2eb
Ebenezer Isaac <mail@ebenezer-isaac.com>
2020-10-14 13:33:47 +0530

    camera integration

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame, sys
 import pygame.locals
 import RPi.GPIO as GPIO
 from time import sleep
+import picamera
+import pygame
+import io
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -19,9 +22,21 @@ wheel3_pwm.start(0)
 wheel4_pwm.start(0)
 
 
+
 pygame.init()
 print("initialized")
-screen = pygame.display.set_mode((200,200))
+screen = pygame.display.set_mode((320,240))
+
+camera = picamera.PiCamera()
+camera.rotation = 180
+camera.resolution = (320, 240)
+camera.crop = (0.0, 0.0, 1.0, 1.0)
+
+x = (screen.get_width() - camera.resolution[0]) / 2
+y = (screen.get_height() - camera.resolution[1]) / 2
+
+rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
+
 running = True
 direction = 0
 speed = 1
@@ -106,4 +121,17 @@ while running:
     	wheel2_pwm.ChangeDutyCycle(0)
     	wheel3_pwm.ChangeDutyCycle(0)
     	wheel4_pwm.ChangeDutyCycle(0)
-
+    stream = io.BytesIO()
+    camera.capture(stream, use_video_port=True, format='rgb')
+    stream.seek(0)
+    stream.readinto(rgb)
+    stream.close()
+    img = pygame.image.frombuffer(rgb[0:
+          (camera.resolution[0] * camera.resolution[1] * 3)],
+           camera.resolution, 'RGB')
+    screen.fill(0)
+    if img:
+        screen.blit(img, (x,y))
+    pygame.display.update()
+camera.close()
+pygame.display.quit()
