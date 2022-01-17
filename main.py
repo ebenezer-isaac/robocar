5a5afcf2680910ef3ade297d0f129a487f91df00
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2022-01-17 22:52:31 +0530

    fixing indentation

--- main.py
+++ main.py
@@ -46,51 +46,51 @@ while running:
             running = False
     keys = pygame.key.get_pressed()
     if keys[pygame.K_PLUS]:
-    	if speed <5:
-    		speed = speed + 1
+        if speed <5:
+            speed = speed + 1
         print(speed)
     if keys[pygame.K_MINUS]:
-    	if speed >1:
-    		speed = speed - 1
+        if speed >1:
+            speed = speed - 1
         print(speed)
     if keys[pygame.K_w] and keys[pygame.K_a]:
-    	direction = 0
-    	wheel1_pwm.ChangeDutyCycle(0)
-    	wheel2_pwm.ChangeDutyCycle(0)
-    	wheel3_pwm.ChangeDutyCycle(100)
-    	wheel4_pwm.ChangeDutyCycle(0)
+        direction = 0
+        wheel1_pwm.ChangeDutyCycle(0)
+        wheel2_pwm.ChangeDutyCycle(0)
+        wheel3_pwm.ChangeDutyCycle(100)
+        wheel4_pwm.ChangeDutyCycle(0)
     elif keys[pygame.K_w] and keys[pygame.K_d]:
-    	direction = 0
-    	wheel1_pwm.ChangeDutyCycle(100)
-    	wheel2_pwm.ChangeDutyCycle(0)
-    	wheel3_pwm.ChangeDutyCycle(0)
-    	wheel4_pwm.ChangeDutyCycle(0)
+        direction = 0
+        wheel1_pwm.ChangeDutyCycle(100)
+        wheel2_pwm.ChangeDutyCycle(0)
+        wheel3_pwm.ChangeDutyCycle(0)
+        wheel4_pwm.ChangeDutyCycle(0)
     elif keys[pygame.K_s] and keys[pygame.K_a]:
-    	direction = 0
-    	wheel1_pwm.ChangeDutyCycle(0)
-    	wheel2_pwm.ChangeDutyCycle(5)
-    	wheel3_pwm.ChangeDutyCycle(0)
-    	wheel4_pwm.ChangeDutyCycle(100)
+        direction = 0
+        wheel1_pwm.ChangeDutyCycle(0)
+        wheel2_pwm.ChangeDutyCycle(5)
+        wheel3_pwm.ChangeDutyCycle(0)
+        wheel4_pwm.ChangeDutyCycle(100)
     elif keys[pygame.K_s] and keys[pygame.K_d]:
-    	direction = 0
-    	wheel1_pwm.ChangeDutyCycle(0)
-    	wheel2_pwm.ChangeDutyCycle(100)
-    	wheel3_pwm.ChangeDutyCycle(0)
-    	wheel4_pwm.ChangeDutyCycle(5)
+        direction = 0
+        wheel1_pwm.ChangeDutyCycle(0)
+        wheel2_pwm.ChangeDutyCycle(100)
+        wheel3_pwm.ChangeDutyCycle(0)
+        wheel4_pwm.ChangeDutyCycle(5)
     elif keys[pygame.K_w]:
-    	direction = 0
-    	wheel1_pwm.ChangeDutyCycle(100)
-    	wheel2_pwm.ChangeDutyCycle(0)
-    	wheel3_pwm.ChangeDutyCycle(100)
-    	wheel4_pwm.ChangeDutyCycle(0)
+        direction = 0
+        wheel1_pwm.ChangeDutyCycle(100)
+        wheel2_pwm.ChangeDutyCycle(0)
+        wheel3_pwm.ChangeDutyCycle(100)
+        wheel4_pwm.ChangeDutyCycle(0)
     elif keys[pygame.K_s]:
-    	direction = 1
-    	wheel1_pwm.ChangeDutyCycle(0)
-    	wheel2_pwm.ChangeDutyCycle(100)
-    	wheel3_pwm.ChangeDutyCycle(0)
-    	wheel4_pwm.ChangeDutyCycle(100)
+        direction = 1
+        wheel1_pwm.ChangeDutyCycle(0)
+        wheel2_pwm.ChangeDutyCycle(100)
+        wheel3_pwm.ChangeDutyCycle(0)
+        wheel4_pwm.ChangeDutyCycle(100)
     elif keys[pygame.K_a]:
-    	if direction==0:
+        if direction==0:
             wheel1_pwm.ChangeDutyCycle(0)
             wheel2_pwm.ChangeDutyCycle(100)
             wheel3_pwm.ChangeDutyCycle(100)
@@ -101,7 +101,7 @@ while running:
             wheel3_pwm.ChangeDutyCycle(0)
             wheel4_pwm.ChangeDutyCycle(100)
     elif keys[pygame.K_d]:
-    	if direction==0:
+        if direction==0:
             wheel1_pwm.ChangeDutyCycle(100)
             wheel2_pwm.ChangeDutyCycle(0)
             wheel3_pwm.ChangeDutyCycle(0)
@@ -112,15 +112,15 @@ while running:
             wheel3_pwm.ChangeDutyCycle(100)
             wheel4_pwm.ChangeDutyCycle(0)
     elif keys[pygame.K_SPACE]:
-    	wheel1_pwm.ChangeDutyCycle(0)
-    	wheel2_pwm.ChangeDutyCycle(0)
-    	wheel3_pwm.ChangeDutyCycle(0)
-    	wheel4_pwm.ChangeDutyCycle(0)
+        wheel1_pwm.ChangeDutyCycle(0)
+        wheel2_pwm.ChangeDutyCycle(0)
+        wheel3_pwm.ChangeDutyCycle(0)
+        wheel4_pwm.ChangeDutyCycle(0)
     else:
-    	wheel1_pwm.ChangeDutyCycle(0)
-    	wheel2_pwm.ChangeDutyCycle(0)
-    	wheel3_pwm.ChangeDutyCycle(0)
-    	wheel4_pwm.ChangeDutyCycle(0)
+        wheel1_pwm.ChangeDutyCycle(0)
+        wheel2_pwm.ChangeDutyCycle(0)
+        wheel3_pwm.ChangeDutyCycle(0)
+        wheel4_pwm.ChangeDutyCycle(0)
     stream = io.BytesIO()
     camera.capture(stream, use_video_port=True, format='rgb')
     stream.seek(0)
